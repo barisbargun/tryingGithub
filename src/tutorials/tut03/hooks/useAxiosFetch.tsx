6c50488767eca039f69c3ba8077b5ddce8ea5735
barış olgun <ben_8_10@hotmail.com>
2023-12-20 21:53:23 +0300

    third-commit

--- src/tutorials/tut03/hooks/useAxiosFetch.tsx
+++ src/tutorials/tut03/hooks/useAxiosFetch.tsx
@@ -1,21 +1,15 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import AxiosOp from "../AxiosOp";
-import axios from "axios";
 
 
 const useAxiosFetch = (url: string) => {
     const [posts, setPosts] = useState<IPost[]>([]);
-    const [isLoaded, setIsLoaded] = useState<boolean>(false);
-    const [error, setError] = useState<boolean>(false);
 
     const getPosts = async () => {
-        setIsLoaded(true);
         console.log("çağrıldı1")
         try {
             setPosts((await AxiosOp.get(url)).data);
-            setError(false);
         } catch (error: any) {
-            setError(error.message);
         }
     }
     
